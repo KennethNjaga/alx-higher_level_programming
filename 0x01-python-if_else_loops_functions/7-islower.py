0b030837ea2332c6311dd4856706ada32f7e2231
KennethNjaga <101345055+KennethNjaga@users.noreply.github.com>
2022-08-23 21:55:51 +0300

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def islower(c):
-    if (ord(c) >= 97 and ord(c) <= 122):
-        return (True)
-    return (False)
+    c = ord(c)
+    return (c >= ord('a') and c <= ord('z'))
